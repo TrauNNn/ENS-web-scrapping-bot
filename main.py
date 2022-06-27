da79875cbd2acc7dda0e8aa3cc2ef45778d55fe9
TrauNNn <s.borodin.work@gmail.com>
2022-06-27 14:59:55 +0500

    The first version

--- main.py
+++ main.py
@@ -7,25 +7,23 @@ import pyodbc
 import telebot
 from telebot import types
 
-import traceback
-import random
+import os
 import time
 import threading
 import json
 import datetime
-from datetime import date
 
 
-def conn_telegram_api(key): #'2099275622:AAHPA1vuU84HiK9xQf9RkvGLSAAHrbJ1HLM'
+def get_telegram_keyboards():
     keyboards = {}
     data = json.load(open("keyboards.json", "r"))
     for keyboard_name in data:
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for key_name in data:
-            key = types.KeyboardButton(text=data[key_name])
+        for key_name in data[keyboard_name]:
+            key = types.KeyboardButton(text=key_name)
             keyboard.add(key)
-        keyboards[keyboard_name] = {keyboard_name: keyboard}
-    return telebot.TeleBot(key, threaded=True), keyboards
+        keyboards[keyboard_name] = keyboard
+    return keyboards
 
 
 def double_split(text, key1, key2):
@@ -33,30 +31,25 @@ def double_split(text, key1, key2):
     return text.split(key1)[1].split(key2)[0]
 
 
-def time_comparing_minutes(time_for_comparison):
-    time_hours, time_minutes = time_for_comparison.split(':')
-    now = datetime.datetime.now()
-    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
-    minutes_from_midnight = int(time_hours)*60 + int(time_minutes)
-    minutes_now = round((now - midnight).seconds / 60)
-    return minutes_from_midnight - minutes_now
-
-
 def conn_to_local_database():
-    return pyodbc.connect(
+    db_cursor = pyodbc.connect(
         r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-        r"DBQ=C:\Users\Administrator\Desktop\ethereum-domains-bot\updated\domains.accdb;"
+        rf"DBQ={os.path.dirname(os.path.realpath(__file__))}/domains.accdb;"
     ).cursor()
+    print("connected")
+    return db_cursor
 
 
 def execute_query(db_cursor, query_name: str, query_args: tuple, output: bool):
+    query_name = query_name.replace("'", "")
     if output:
         db_cursor.execute("DROP TABLE output")
     db_cursor.execute("{CALL " + query_name + " " + str(query_args) + "}")
     db_cursor.commit()
     if output:
         db_cursor.execute("SELECT * FROM output")
-        return db_cursor.fetchall()
+        to_return = db_cursor.fetchall()
+        return to_return if len(to_return) != 1 else to_return[0]
     return True
 
 
@@ -65,13 +58,13 @@ def get_selenium_browser():
     options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
     browser = webdriver.Chrome(ChromeDriverManager().install())
     browser.get('https://app.ens.domains/')
-    time.sleep(3)
+    time.sleep(4)
     return browser
 
 
 def search_for_domain(domain_name, browser, db_cursor):
     browser.get('https://app.ens.domains/')
-    time.sleep(3)
+    time.sleep(4)
 
     html_of_profile = browser.page_source
     soup = BeautifulSoup(str(html_of_profile), 'lxml')
@@ -79,17 +72,21 @@ def search_for_domain(domain_name, browser, db_cursor):
     quotes = browser.find_elements_by_xpath("//input[@placeholder='Search names or addresses']")
 
     if len(quotes) == 0:
-        time.sleep(4)
+        time.sleep(3)
         return "the page is not ready, correct time.sleep amounts or check your internet connection"
 
     quotes[0].click()
-    quotes[0].send_keys(domain_name + Keys.ENTER)
-    time.sleep(3)
-    status = get_domain(browser.page_source, db_cursor, domain_name)
-    return status
+    quotes[0].send_keys(domain_name)
+    time.sleep(2)
+    quotes[0].send_keys(Keys.ENTER)
+    time.sleep(7)
+    try:
+        get_domain_into_database(browser.page_source, db_cursor, domain_name)
+    except:
+        return "the page is not ready, correct time.sleep amounts or check your internet connection"
 
 
-def get_domain(html_of_profile, db_cursor, domain_name):
+def get_domain_into_database(html_of_profile, db_cursor, domain_name):
     soup = BeautifulSoup(str(html_of_profile), 'lxml')
     quotes = soup.find_all('div', class_='css-0')  # css-0 e1736otp6
 
@@ -107,162 +104,101 @@ def get_domain(html_of_profile, db_cursor, domain_name):
                 outdated_day = double_split(quotes[0].text, 'Expires ', ' at').replace(' ', '')
 
         if outdated_day is not None:
-            outdated_day = outdated_day.split('.')[1] + '/' + outdated_day.split('.')[2] + '/' + outdated_day.split('.')[0]
+            outdated_day = outdated_day.split('.')[1] + '/' + outdated_day.split('.')[2] + '/' + \
+                           outdated_day.split('.')[0]
             outdated_time = double_split(quotes[0].text, 'at ', '(UTC').replace(' ', '')
-            if len(outdated_time.split(':')[0]) < 2:
-                outdated_time = '0' + outdated_time
-            if len(outdated_time.split(':')[1]) < 2:
-                outdated_time = outdated_time.split(':')[0] + ':' + '0' + outdated_time.split(':')[1]
 
         if outdated_time is not None:
-            query_args = (outdated_day, outdated_time, False, False, domain_name, False)
-            execute_query(db_cursor, "update_domains_data", query_args, False)
+            query_args = (outdated_day, outdated_time, 0, 0, domain_name)
+            execute_query(db_cursor, "update_domain_data", query_args, False)
         else:
-            return "expiry time or date is not found"
-        return 'Unavailable'
+            pass
 
     if quotes[0].text == 'Available':
-        premium_required = True if "Available with a temporary premium" in str(soup) else False
-        query_args = ("Date()",
-                      f"'{datetime.datetime.now().hour}:{datetime.datetime.now().minute}'",
-                      True,
-                      True,
-                      premium_required,
-                      domain_name)
-        execute_query(db_cursor, "update_domains_data", query_args, False)
-        return "premium required" if premium_required else "Available"
+        query_args = (str(datetime.date.today()), f"00:00", -1, -1, domain_name)
+        execute_query(db_cursor, "update_domain_data", query_args, False)
 
 
-def get_notifications(db_cursor, browser):
-    domains = []
-    raw_data = execute_query(db_cursor, "find_domains_with_notification", (), True)
-    for domain_id in range(0, len(raw_data)):
-        if time_comparing_minutes(raw_data[domain_id][1]) < 12:
-            search_for_domain(raw_data[domain_id][0], browser, db_cursor)
-            updated_time = execute_query(db_cursor, "time_by_domain_name", (raw_data[domain_id][0]), True)[0]
-            if time_comparing_minutes(updated_time[0]) < 12:
-                domains.append((raw_data[domain_id][0], raw_data[domain_id][2]))
-                execute_query(db_cursor, "set_domain_as_not_checked", (raw_data[domain_id][0]), False)
-
-    return domains
-
-
-def get_domains_close_to_time(db_cursor):
-    raw_data = execute_query(db_cursor, "get_domains_close_to_expiry", (), True)
-    for domain_name in range(0, len(raw_data) ):
-        raw_data[domain_name] = raw_data[domain_name][0]
-        
-    return raw_data
-
-
-def update_domains_status(db_cursor):
-    db_cursor.execute(f"UPDATE domains SET IsChecked = False, OutdatedDay = Date() WHERE OutdatedDay < Date()")
-    db_cursor.commit()
-    db_cursor.execute(f"SELECT domainName FROM domains WHERE (IsChecked = False AND OutdatedDay = Date() )")
-
-
-def add_domains_to_database(list_of_domains, db_cursor):
-    to_return = []
-    for to_push in list_of_domains:
-        to_push = to_push.replace(' ', '').replace('\n', '')
-        db_cursor.execute(f"SELECT domainName FROM domains WHERE ( domainName = '{to_push}')")
-        if len(db_cursor.fetchall() ) == 0 and len(to_push) > 2:
-            db_cursor.execute(f"INSERT INTO domains VALUES ('{to_push}', False, Date(), '0:00', False, False, False)")
-        db_cursor.commit()
-    return to_return
-
-
-def remove_domains_from_database(list_of_domains, db_cursor):
-    for to_remove in list_of_domains:
-        to_remove = to_remove.replace(' ', '').replace('\n', '')
-        db_cursor.execute(f"SELECT domainName FROM domains WHERE ( domainName = '{to_remove}')")
-        if len(db_cursor.fetchall() ) == 1:
-            db_cursor.execute(f"DELETE FROM domains WHERE ( domainName = '{to_remove}')")
-    db_cursor.commit()
-
-
-bot, keyboards = conn_telegram_api("2099275622:AAHPA1vuU84HiK9xQf9RkvGLSAAHrbJ1HLM")
+key = ''  # get it from config
+bot = telebot.TeleBot(key, threaded=False)
+database_cursor = conn_to_local_database()
+mode = None
 
 
 @bot.message_handler(content_types=['text'])
-def message_get(message, db_cursor, keyboard):
-    db_cursor.execute(f"SELECT eventtype, answer, header_file, key FROM telebot_events WHERE key = '{message.text}'")
-    telebot_event = db_cursor.fetchone()
+def message_get(message):
+    db_cursor = conn_to_local_database()
+    try:
+        telebot_event = execute_query(db_cursor, "get_event", ("'" + message.text + "'"), True)
 
-    if telebot_event == None:
-        db_cursor.execute(f"SELECT nextaction, hasaccess FROM users WHERE tgID = {message.from_user.id}")
-        user_info = db_cursor.fetchone()
-        if not (user_info[1]):
-            bot.send_message(message.from_user.id,
-                             'Access denied')
-            return None
+        global mode
+        if not len(telebot_event):
+            if mode is not None:
+                query_args = message.text.split(' ')
+                text_to_send = 'query succeed with:\n'
 
-        if user_info[0] == 'Select domains with notification':
-            domains = message.text.split(' ')
-            for domain in domains:
-                try:
-                    db_cursor.execute(
-                        f"INSERT INTO domains VALUES ('{domain}', False, Date(), '00:00', False, {message.from_user.id}, True)")
-                except:
-                    db_cursor.execute(f"UPDATE domains SET IsSelected = True WHERE domainName = '{domain}'")
-        if user_info[0] == 'Remove notification from domains':
-            domains = message.text.split(' ')
-            for domain in domains:
-                try:
-                    db_cursor.execute(
-                        f"INSERT INTO domains VALUES ('{domain}', False, Date(), '00:00', False, {message.from_user.id}, False)")
-                except:
-                    db_cursor.execute(f"UPDATE domains SET IsSelected = False WHERE domainName = '{domain}'")
-        db_cursor.commit()
-        return None
+                for arg in query_args:
+                    if execute_query(db_cursor, mode, ("'" + arg + "'"), False):
+                        text_to_send += arg + ', '
+                bot.send_message(message.from_user.id,
+                                 text_to_send,
+                                 reply_markup=keyboards["menu"])
+                return True
 
-    text_to_send = telebot_event[1] + '\n'
-    send_with_file = False
+            else:
+                bot.send_message(message.from_user.id,
+                                 "wrong command, or you have not chosen action",
+                                 reply_markup=keyboards["menu"])
+                return False
 
-    if telebot_event[0] == 'get':
-        file_to_send = open(f'{telebot_event[1]}.csv', 'w')
-        db_cursor.execute(f"SELECT result FROM {message.text.replace(' ', '_')}")
-        raw_data = db_cursor.fetchall()
-        file_to_send.write(telebot_event[2] + '\n')
+        text_to_send = telebot_event[1] + '\n'
+        mode = None if telebot_event[0] != 'execute' else telebot_event[4].replace(" ", "_")
 
-        for cell in raw_data:
-            try:
-                print(str(cell))
-                if len(str(cell).split(',')[2].split(':')[0]) < 2:
-                    cell_arr = str(cell)[0].split(',')
-                    cell = cell_arr[0] + ',' + cell_arr[1] + ',0' + cell_arr[2].split(':')[0] + ':' + \
-                           cell_arr[2].split(':')[1] + ',' + cell_arr[3] + ',' + cell_arr[4]
-                if len(str(cell).split(',')[2].split(':')[1]) < 2:
-                    cell_arr = str(cell)[0].split(',')
-                    cell = cell_arr[0] + ',' + cell_arr[1] + ',' + cell_arr[2].split(':')[0] + ':0' + \
-                           cell_arr[2].split(':')[1] + ',' + cell_arr[3] + ',' + cell_arr[4]
-            except:
-                pass
-            file_to_send.write(cell[0].replace('\n', '').replace(',-1..', ',Yes').replace(',0..', ',No') + '\n')
-        file_to_send.close()
-        send_with_file = True
+        if telebot_event[0] == 'get':
+            file_to_send = open(f'{telebot_event[1]}.csv', 'w')
+            raw_data = execute_query(db_cursor, message.text.replace(' ', '_'), (), True)
+            file_to_send.write(telebot_event[2] + '\n')
 
-    if telebot_event[0] == 'execute':
-        try:
-            db_cursor.execute(f"INSERT INTO users VALUES ({message.from_user.id}, False, '{telebot_event[3]}')")
-        except:
-            db_cursor.execute(
-                f"UPDATE users SET nextaction='{telebot_event[3]}' WHERE tgID = {message.from_user.id}")
-        db_cursor.commit()
+            for cell in raw_data:
+                file_to_send.write(str(cell[0]).replace('\n', '').replace(',-1..', ',Yes').replace(',0..', ',No') + '\n')
+                """
+                cell has type list with never more than 1 element, 
+                where the string object contains at 0 place.
+                -1 and 0 are True and False in MS Access, 
+                two dots are written in queries to make python recognize -1 and 0 as their bool values.
+                """
+            file_to_send.close()
+            bot.send_document(message.from_user.id,
+                              open(f'{telebot_event[1]}.csv'),
+                              reply_markup=keyboards["menu"])
+            return True
 
-    if send_with_file:
-        bot.send_document(message.from_user.id,
-                          open(f'{telebot_event[1]}.csv'),
-                          reply_markup=keyboard)
+        bot.send_message(message.from_user.id, text_to_send, reply_markup=keyboards["menu"])
+        time.sleep(1)
+        return True
 
-    else:
-        bot.send_message(message.from_user.id,
-                         text_to_send,
-                         reply_markup=keyboard)
+    except:
+        bot.send_message(message.from_user.id, "Something went wrong, check your message is it correct",
+                         reply_markup=keyboards["menu"])
+        return False
+
+
+def worker(browser, db_cursor):
+    execute_query(db_cursor, "set_domains_unchecked", (), False)
+    to_parse = execute_query(db_cursor, "get_domains_close_to_expiry", (), True)
+    for domain_name in range(0, len(to_parse)):
+        search_for_domain(to_parse[domain_name][0], browser, db_cursor)
 
 
 if __name__ == "__main__":
-    get_selenium_browser()
-    conn_to_local_database()
-    #search_for_domain(domain_name, browser, db_cursor)
+    keyboards = get_telegram_keyboards()
+    web_browser = get_selenium_browser()
 
+    backend = threading.Thread(target=worker, args=(web_browser, conn_to_local_database()))
+    telegram_bot = threading.Thread(target=bot.infinity_polling)
+
+    telegram_bot.start()
+    backend.start()
+
+    telegram_bot.join()
+    backend.join()
