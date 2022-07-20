74819ac049841e1886f38dfcb7b5c4dc0d9a8502
TrauNNn <s.borodin.work@gmail.com>
2022-07-20 10:56:49 +0500

    Added notifications, support text files input Improved readability of code - now there are comments of functions

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ import datetime
 
 
 def get_telegram_keyboards():
+    """
+    returns dictionary with keyboard objects for bot.send_message methods
+    """
     keyboards = {}
     data = json.load(open("keyboards.json", "r"))
     for keyboard_name in data:
@@ -26,34 +29,58 @@ def get_telegram_keyboards():
     return keyboards
 
 
-def double_split(text, key1, key2):
+def double_split(text: str, key1: str, key2: str):
+    """
+    method is used for selection needed str fragment.
+    '' as keys if you need to throw away only right (key1='') or left (key2='') part of text
+    """
     text = str(text)
     return text.split(key1)[1].split(key2)[0]
 
 
 def conn_to_local_database():
+    """
+    returns cursor
+    database must be in the same directory with main.py
+    """
     db_cursor = pyodbc.connect(
         r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
         rf"DBQ={os.path.dirname(os.path.realpath(__file__))}/domains.accdb;"
     ).cursor()
-    print("connected")
+    print('connected')
     return db_cursor
 
 
-def execute_query(db_cursor, query_name: str, query_args: tuple, output: bool):
-    query_name = query_name.replace("'", "")
-    if output:
-        db_cursor.execute("DROP TABLE output")
-    db_cursor.execute("{CALL " + query_name + " " + str(query_args) + "}")
-    db_cursor.commit()
-    if output:
-        db_cursor.execute("SELECT * FROM output")
-        to_return = db_cursor.fetchall()
-        return to_return if len(to_return) != 1 else to_return[0]
-    return True
+def execute_query(query_name: str, query_args: tuple, output: bool):
+    """
+    executes stored procedures
+    if output is True returns the list of results
+    will not work correctly if stored procedure returns 2 or more columns
+    """
+    try:
+        db_cursor = conn_to_local_database()
+        query_name = query_name.replace("'", "")
+        db_cursor.execute("{CALL " + query_name + " " + str(query_args) + "}")
+        db_cursor.commit()
+
+        if output:
+            db_cursor.execute(f"SELECT * FROM " + query_name + "_output")
+            to_return = db_cursor.fetchall()
+            db_cursor.execute(f"DROP TABLE {query_name}_output")
+            db_cursor.commit()
+            db_cursor.close()
+            return to_return if len(to_return) != 1 else to_return[0]
+
+        db_cursor.close()
+        return True
+    except:
+        return () if output else False
 
 
 def get_selenium_browser():
+    """
+    returns chrome browser with opened ENS website main page
+    """
     options = Options()
     options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
     browser = webdriver.Chrome(ChromeDriverManager().install())
@@ -62,31 +89,34 @@ def get_selenium_browser():
     return browser
 
 
-def search_for_domain(domain_name, browser, db_cursor):
+def search_for_domain(domain_name: str, browser):
+    """
+    returns html source of page with domain info
+    change time.sleep args consider your internet connection
+    """
     browser.get('https://app.ens.domains/')
-    time.sleep(4)
-
-    html_of_profile = browser.page_source
-    soup = BeautifulSoup(str(html_of_profile), 'lxml')
-
+    time.sleep(7)
     quotes = browser.find_elements_by_xpath("//input[@placeholder='Search names or addresses']")
 
     if len(quotes) == 0:
-        time.sleep(3)
+        time.sleep(4)
         return "the page is not ready, correct time.sleep amounts or check your internet connection"
 
     quotes[0].click()
     quotes[0].send_keys(domain_name)
-    time.sleep(2)
+    time.sleep(3)
     quotes[0].send_keys(Keys.ENTER)
-    time.sleep(7)
+    time.sleep(9)
     try:
-        get_domain_into_database(browser.page_source, db_cursor, domain_name)
+        get_domain_info(browser.page_source, domain_name)
     except:
         return "the page is not ready, correct time.sleep amounts or check your internet connection"
 
 
-def get_domain_into_database(html_of_profile, db_cursor, domain_name):
+def get_domain_info(html_of_profile, domain_name: str):
+    """
+    gets domain info from it's page html source code into database
+    """
     soup = BeautifulSoup(str(html_of_profile), 'lxml')
     quotes = soup.find_all('div', class_='css-0')  # css-0 e1736otp6
 
@@ -110,27 +140,29 @@ def get_domain_into_database(html_of_profile, db_cursor, domain_name):
 
         if outdated_time is not None:
             query_args = (outdated_day, outdated_time, 0, 0, domain_name)
-            execute_query(db_cursor, "update_domain_data", query_args, False)
+            execute_query("update_domain_data", query_args, False)
         else:
             pass
 
     if quotes[0].text == 'Available':
         query_args = (str(datetime.date.today()), f"00:00", -1, -1, domain_name)
-        execute_query(db_cursor, "update_domain_data", query_args, False)
+        execute_query("update_domain_data", query_args, False)
 
 
-key = ''  # get it from config
+key = ''  # in future get it from config
 bot = telebot.TeleBot(key, threaded=False)
-database_cursor = conn_to_local_database()
 mode = None
 
 
 @bot.message_handler(content_types=['text'])
-def message_get(message):
-    db_cursor = conn_to_local_database()
+def message_got(message):
+    """
+    bot method, returns False if not succeed with user's query
+    firstly checks is message a command,
+    if it's not, tries to complete last 'execute' query (mode) with given message as domains list
+    """
     try:
-        telebot_event = execute_query(db_cursor, "get_event", ("'" + message.text + "'"), True)
-
+        telebot_event = execute_query("get_event", ("'" + message.text + "'"), True)
         global mode
         if not len(telebot_event):
             if mode is not None:
@@ -138,7 +170,12 @@ def message_get(message):
                 text_to_send = 'query succeed with:\n'
 
                 for arg in query_args:
-                    if execute_query(db_cursor, mode, ("'" + arg + "'"), False):
+                    arg = arg.replace(" ", ";;")
+                    arg = arg.replace("\n", ";;")
+                    arg = arg.replace("\r", "")
+                    arg = arg.replace(",", ";;")
+                    arg = arg.replace(";;;;", ";;")
+                    if execute_query(mode, ("'" + arg + "'"), False):
                         text_to_send += arg + ', '
                 bot.send_message(message.from_user.id,
                                  text_to_send,
@@ -156,7 +193,7 @@ def message_get(message):
 
         if telebot_event[0] == 'get':
             file_to_send = open(f'{telebot_event[1]}.csv', 'w')
-            raw_data = execute_query(db_cursor, message.text.replace(' ', '_'), (), True)
+            raw_data = execute_query(message.text.replace(' ', '_'), (), True)
             file_to_send.write(telebot_event[2] + '\n')
 
             for cell in raw_data:
@@ -174,7 +211,6 @@ def message_get(message):
             return True
 
         bot.send_message(message.from_user.id, text_to_send, reply_markup=keyboards["menu"])
-        time.sleep(1)
         return True
 
     except:
@@ -183,18 +219,79 @@ def message_get(message):
         return False
 
 
-def worker(browser, db_cursor):
-    execute_query(db_cursor, "set_domains_unchecked", (), False)
-    to_parse = execute_query(db_cursor, "get_domains_close_to_expiry", (), True)
-    for domain_name in range(0, len(to_parse)):
-        search_for_domain(to_parse[domain_name][0], browser, db_cursor)
+@bot.message_handler(content_types=['document'])
+def file_got(message):
+    """
+    same with message_got, but only for completing 'execute' queries (mode)
+    with domains list inside the file
+    """
+    global mode
+    if mode is not None:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open('user_file', 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        text_to_send = 'query succeed with:\n'
+        for args in open('user_file'):
+            args = args.replace(" ", ";;")
+            args = args.replace("\n", ";;")
+            args = args.replace("\r", "")
+            args = args.replace(",", ";;")
+            args = args.replace(";;;;", ";;")
+            args = args.split(';;')
+            for arg in args:
+                if execute_query(mode, ("'" + arg + "'"), False):
+                    text_to_send += arg + ', '
+        bot.send_message(message.from_user.id,
+                         text_to_send,
+                         reply_markup=keyboards["menu"])
+        return True
+
+
+def worker(browser):
+    """
+    infinity searching for unchecked domains, parsing them
+    and after that starting notifying users
+    notifications will not work until every domain is not checked
+    """
+    while True:
+        execute_query("put_domains_in_queue", (), False)
+        to_parse = execute_query("get_domains_close_to_expiry", (), True)
+        for domain_name in range(0, len(to_parse)):
+            search_for_domain(to_parse[domain_name][0], browser)
+        notifications_worker()
+
+
+def notifications_worker():
+    """
+    notifying about domains that become available soon
+    every user which telegram account ID is stored in database
+    """
+    users = execute_query("get_all_users", (), True)
+    notify_settings = json.load(open("notifications.json", "r"))
+    for notification in notify_settings:
+        to_send = execute_query("get_name_by_outdate_time", (notification,), True)
+        for domain in to_send:
+            for user in users:
+                bot.send_message(user,
+                                 f'Domain "{domain[0]}" '
+                                 f'is going to become available in '
+                                 f'{notify_settings[notification]}'
+                                 f', if it is not re-bought',
+                                 reply_markup=keyboards["menu"])
+        time.sleep(60)
 
 
 if __name__ == "__main__":
+    """
+    tried to put notification_worker into new thread,
+    but caught lots of pyodbc errors
+    """
     keyboards = get_telegram_keyboards()
     web_browser = get_selenium_browser()
 
-    backend = threading.Thread(target=worker, args=(web_browser, conn_to_local_database()))
+    backend = threading.Thread(target=worker, args=(web_browser,))
     telegram_bot = threading.Thread(target=bot.infinity_polling)
 
     telegram_bot.start()
